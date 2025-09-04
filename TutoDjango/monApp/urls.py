c7d8ecad942fa21c93e312026d68fd5222b5d064
manglij <nicolas.lepage41@gmail.com>
2025-09-04 09:08:46 +0200

    ajout home vide

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -4,6 +4,7 @@ from .views.contact import contact
 from .views.about import about
 
 urlpatterns = [
+    path("home", home, name="home-simple"),
     path("home/<param>", home, name="home"),
     path("contact_us/", contact, name="contact"),
     path("about_us/", about, name="about"),
--- TutoDjango/monApp/views/home.py
+++ TutoDjango/monApp/views/home.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request, param):
+def home(request, param="Django"):
     return HttpResponse(f"<h1>Hello {param}!</h1>")
